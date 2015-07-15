06a4ece5abce38bf05eb91af9b1f49227a93faa4
Fran Dios <frankdiox@gmail.com>
2015-07-15 16:25:10 +0900

    Initial app

---
+++ js/controllers.js
@@ -0,0 +1,43 @@
+
+angular.module('myApp')
+
+// Controller
+
+.controller('VideosController', function ($scope, $http, $log, VideosService) {
+
+    init();
+
+    function init() {
+      $scope.youtube = VideosService.getYoutube();
+      $scope.results = VideosService.getResults();
+      $scope.history = VideosService.getHistory();
+    }
+
+    $scope.launch = function (id, title, archive) {
+      VideosService.launchPlayer(id, title);
+      if (archive) {
+      	VideosService.archiveVideo(id, title);
+      }
+      $log.info('Launched id:' + id + ' and title:' + title);
+    };
+
+    $scope.search = function () {
+      $http.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: 'AIzaSyDiByKCET1fLAuBHJL462BXx2lnKXce6so',
+          type: 'video',
+          maxResults: '8',
+          part: 'id,snippet',
+          fields: 'items/id,items/snippet/title,items/snippet/description,items/snippet/thumbnails/default,items/snippet/channelTitle',
+          q: this.query
+        }
+      })
+      .success( function (data) {
+        VideosService.listResults(data);
+        $log.info(data);
+      })
+      .error( function () {
+        $log.info('Search error');
+      });
+    }
+});
