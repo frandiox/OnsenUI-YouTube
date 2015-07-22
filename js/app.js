d73f348b078d5af246e3d4831b60ddf54e2509be
Fran Dios <frankdiox@gmail.com>
2015-07-22 15:04:50 +0900

    Mixed Content fix for Chrome

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var app = angular.module('myApp', ['onsen']);
 
 app.run(function () {
   var tag = document.createElement('script');
-  tag.src = "http://www.youtube.com/iframe_api";
+  tag.src = "https://www.youtube.com/iframe_api";
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 });
